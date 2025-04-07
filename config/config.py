18c40e5d7471c78be4c002c00da8a74aa5aac730
jamal_aldeen_khalil <66186669+jamal-kh@users.noreply.github.com>
2025-04-07 20:25:11 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 Configs = {
     "videos_path" : "./public/videos",
     "audios_path" : "./public/audios",
-    "bot_token" : "8004328546:AAEt36bwv80fwyls1fiE3fBDthig0pveP44"
+    "bot_token" : ""
 }
